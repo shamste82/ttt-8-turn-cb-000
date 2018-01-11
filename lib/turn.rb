b80572528c3c34cce92b57d414737d5c1df78d7e
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-01-11 20:11:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,8 @@
 def display_board(board)
-  puts "___________ "
+  puts "-----------"
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "___________ "
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "___________ "
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
