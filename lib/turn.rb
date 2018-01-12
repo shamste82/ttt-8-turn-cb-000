fb44000128711626b48b53e67ea45ee296797136
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-01-12 13:02:21 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,21 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def position_taken(board, index):
+  case board[index]:
+  when "X"
+    return false
+  when "O"
+    return false
+  else:
+    return true
+end
+
+def valid_move(board, index)
+  if index < 9 and index >= 0:
+    if position_taken(board, index):
+      return true
+  else:
+    return false
+end
